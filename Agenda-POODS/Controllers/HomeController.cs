7c294aff71e210628108ac9a5a75394e11da7238
André do Valle Medeiros <andre.v.medeiros@hotmail.com>
2023-10-01 21:09:38 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda-POODS/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Agenda_POODS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Agenda_POODS.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();   
+
+            home.Nome = "André Medeiros";
+            home.Email = "hmdre@gmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
